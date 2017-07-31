174f6609d4928801350d330a88ac939384403921
Catalin Lazar <catalin.lazar@endava.com>
2017-07-31 14:29:29 +0300

    Add edge of field

--- src/main/java/ro/contezi/paperfootball/FootballField.java
+++ src/main/java/ro/contezi/paperfootball/FootballField.java
@@ -1,5 +1,6 @@
 package ro.contezi.paperfootball;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -7,11 +8,21 @@ import java.util.stream.IntStream;
 public class FootballField {
     private final Set<Point> goalNorth;
     private final Set<Point> goalSouth;
+    private final Set<Point> edge;
     
     public FootballField(int width, int height) {
-        int goalHeight = height / 2 + 1;
+        int halfWidth = width / 2;
+        int halfHeight = height / 2;
+        int goalHeight = halfHeight + 1;
         goalNorth = IntStream.range(-1, 2).mapToObj(x -> new Point(x, goalHeight)).collect(Collectors.toSet());
         goalSouth = IntStream.range(-1, 2).mapToObj(x -> new Point(x, -goalHeight)).collect(Collectors.toSet());
+        edge = new HashSet<>();
+        IntStream.range(-halfWidth, halfWidth + 1).mapToObj(x -> new Point(x, halfHeight)).forEach(edge::add);
+        IntStream.range(-halfWidth, halfWidth + 1).mapToObj(x -> new Point(x, -halfHeight)).forEach(edge::add);
+        IntStream.range(-halfHeight, halfHeight +1).mapToObj(y -> new Point(halfWidth, y)).forEach(edge::add);
+        IntStream.range(-halfHeight, halfHeight +1).mapToObj(y -> new Point(-halfWidth, y)).forEach(edge::add);
+        edge.remove(new Point(0, -halfHeight));
+        edge.remove(new Point(0, halfHeight));
     }
 
     public boolean isGoalNorth(Point point) {
@@ -20,5 +31,9 @@ public class FootballField {
     
     public boolean isGoalSouth(Point point) {
         return goalSouth.contains(point);
+    }
+    
+    public boolean isEdge(Point point) {
+        return edge.contains(point);
     }
 }
--- src/test/java/ro/contezi/paperfootball/FootballFieldTest.java
+++ src/test/java/ro/contezi/paperfootball/FootballFieldTest.java
@@ -14,6 +14,11 @@ public class FootballFieldTest {
     }
     
     @Test
+    public void findsNotEdgeOnTheNorthGoalLine() {
+        assertThat(field.isEdge(new Point(0, 5))).isFalse();
+    }
+    
+    @Test
     public void findsGoalNorthCenter() {
         assertThat(field.isGoalNorth(new Point(0, 6))).isTrue();
     }
@@ -34,6 +39,11 @@ public class FootballFieldTest {
     }
     
     @Test
+    public void findsNotEdgeOnTheSouthGoalLine() {
+        assertThat(field.isEdge(new Point(0, -5))).isFalse();
+    }
+    
+    @Test
     public void findsGoalSouthCenter() {
         assertThat(field.isGoalSouth(new Point(0, -6))).isTrue();
     }
@@ -46,5 +56,65 @@ public class FootballFieldTest {
     @Test
     public void findsGoalSouthRight() {
         assertThat(field.isGoalSouth(new Point(1, -6))).isTrue();
+    }
+    
+    @Test
+    public void findsPointOnLeftEdge() throws Exception {
+        assertThat(field.isEdge(new Point(-4, 1))).isTrue();
+    }
+    
+    @Test
+    public void findsPointNearLeftEdge() throws Exception {
+        assertThat(field.isEdge(new Point(-3, 2))).isFalse();
+    }
+    
+    @Test
+    public void findsPointOnRightEdge() throws Exception {
+        assertThat(field.isEdge(new Point(4, 1))).isTrue();
+    }
+    
+    @Test
+    public void findsPointNearRightEdge() throws Exception {
+        assertThat(field.isEdge(new Point(3, -2))).isFalse();
+    }
+    
+    @Test
+    public void findsPointOnBottomLeft() throws Exception {
+        assertThat(field.isEdge(new Point(-3, -5))).isTrue();
+    }
+    
+    @Test
+    public void findsPointNearBottomLeftEdge() throws Exception {
+        assertThat(field.isEdge(new Point(-3, -4))).isFalse();
+    }
+    
+    @Test
+    public void findsPointOnTopLeft() throws Exception {
+        assertThat(field.isEdge(new Point(-3, 5))).isTrue();
+    }
+    
+    @Test
+    public void findsPointNearTopLeftEdge() throws Exception {
+        assertThat(field.isEdge(new Point(-3, 4))).isFalse();
+    }
+    
+    @Test
+    public void findsPointOnBottomRight() throws Exception {
+        assertThat(field.isEdge(new Point(3, -5))).isTrue();
+    }
+    
+    @Test
+    public void findsPointNearBottomRightEdge() throws Exception {
+        assertThat(field.isEdge(new Point(3, -4))).isFalse();
+    }
+    
+    @Test
+    public void findsPointOnTopRight() throws Exception {
+        assertThat(field.isEdge(new Point(3, 5))).isTrue();
+    }
+    
+    @Test
+    public void findsPointNearTopRightEdge() throws Exception {
+        assertThat(field.isEdge(new Point(3, 4))).isFalse();
     }
 }
